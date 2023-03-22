260ce3a46c9d666a34c3a88201f154bb050c1480
AdamWarfa <Adamwarfadk@gmail.com>
2023-03-22 12:44:00 +0100

    potter

--- app.js
+++ app.js
@@ -4,26 +4,131 @@
 window.addEventListener("load", initApp);
 
 function initApp() {
-    // Harry Potter
-    const potterName = "Harry Potter";
-    const potterGender = "male";
-    const potterHouse = "Gryffindor";
-    const potterDateOfBirth = "31-07-1980";
-    const potterAncestry = "half-blood";
-    const potterEyeColour = "green";
-    const potterHairColour = "black";
-    const potterActor = "Daniel Radcliffe";
-    const potterImage = "http://hp-api.herokuapp.com/images/harry.jpg";
+  // Harry Potter
+  const harry = {
+    name: "Harry Potter",
+    species: "human",
+    gender: "male",
+    house: "Gryffindor",
+    dateOfBirth: "31-07-1980",
+    yearOfBirth: 1980,
+    ancestry: "half-blood",
+    eyeColour: "green",
+    hairColour: "black",
+    wand: "holly,phoenix feather,11",
+    patronus: "stag",
+    hogwartsStudent: true,
+    hogwartsStaff: false,
+    actor: "Daniel Radcliffe",
+    alive: true,
+    image: "http://hp-api.herokuapp.com/images/harry.jpg",
+  };
 
-    console.log(
-        potterName,
-        potterGender,
-        potterHouse,
-        potterDateOfBirth,
-        potterAncestry,
-        potterEyeColour,
-        potterHairColour,
-        potterActor,
-        potterImage
-    );
+  // Hermione Granger
+  const hermione = {
+    name: "Hermione Granger",
+    species: "human",
+    gender: "female",
+    house: "Gryffindor",
+    dateOfBirth: "19-09-1979",
+    yearOfBirth: 1979,
+    ancestry: "muggleborn",
+    eyeColour: "brown",
+    hairColour: "brown",
+    wand: "vine,dragon heartstring",
+    patronus: "otter",
+    hogwartsStudent: true,
+    hogwartsStaff: false,
+    actor: "Emma Watson",
+    alive: true,
+    image: "http://hp-api.herokuapp.com/images/hermione.jpeg",
+  };
+
+  // Ron Weasley
+  const ron = {
+    name: "Ron Weasley",
+    species: "human",
+    gender: "male",
+    house: "Gryffindor",
+    dateOfBirth: "01-03-1980",
+    yearOfBirth: 1980,
+    ancestry: "pure-blood",
+    eyeColour: "blue",
+    hairColour: "red",
+    wand: "willow,unicorn tail-hair,14",
+    patronus: "Jack Russell terrier",
+    hogwartsStudent: true,
+    hogwartsStaff: false,
+    actor: "Rupert Grint",
+    alive: true,
+    image: "http://hp-api.herokuapp.com/images/ron.jpg",
+  };
+
+  const draco = {
+    name: "Draco Malfoy",
+    species: "human",
+    gender: "male",
+    house: "Slytherin",
+    dateOfBirth: "05-06-1980",
+    yearOfBirth: 1980,
+    ancestry: "pure-blood",
+    eyeColour: "grey",
+    hairColour: "blonde",
+    wand: "hawthorn,unicorn tail-hair,10",
+    patronus: "",
+    hogwartsStudent: true,
+    hogwartsStaff: false,
+    actor: "Tom Felton",
+    alive: true,
+    image: "http://hp-api.herokuapp.com/images/draco.jpg",
+  };
+  console.log(harry);
+  console.log(ron);
+  console.log(hermione);
+
+  showCharacter(harry);
+  showCharacter(draco);
 }
+
+function showCharacter(character) {
+  console.log(character);
+  document.querySelector("#characters").insertAdjacentHTML(
+    "beforeend",
+    /*html*/ `
+            <article class="grid-item">
+                <img src="${character.image}">
+                <h2>${character.name}</h2>
+                <p>${character.house}</p>
+                <p>Date of Birth: ${character.dateOfBirth}</p>
+                <p>Played by ${character.actor}</p>
+            </article>
+        `
+  );
+}
+
+// function showCharacter(image, name, house, dateOfBirth, actor) {
+//     //define elements
+//     const articleElement = document.createElement("article");
+//     const imageElement = document.createElement("img");
+//     const nameElement = document.createElement("h2");
+//     const houseElement = document.createElement("p");
+//     const dateOfBirthElement = document.createElement("p");
+//     const actorElement = document.createElement("p");
+
+//     // set content
+//     imageElement.src = image;
+//     nameElement.textContent = name;
+//     houseElement.textContent = house;
+//     dateOfBirthElement.textContent = `Date of Birth: ${dateOfBirth}`;
+//     actorElement.textContent = `Played by ${actor}`;
+
+//     // append child elements to articleElement
+//     articleElement.appendChild(imageElement);
+//     articleElement.appendChild(nameElement);
+//     articleElement.appendChild(houseElement);
+//     articleElement.appendChild(dateOfBirthElement);
+//     articleElement.appendChild(actorElement);
+
+//     // append article to grid
+//     document.querySelector("#characters").appendChild(articleElement);
+// }
